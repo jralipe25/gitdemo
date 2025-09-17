cc295779b767a5d86489bb67fa74533e10a68f4d
John Rey Alipe <jralipe@isufst.edu.ph>
2025-09-17 10:17:27 +0800

    created student model and added string name

--- src/main/java/com/ionres/gitdemo/StudentModel.java
+++ src/main/java/com/ionres/gitdemo/StudentModel.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.ionres.gitdemo;
 
 /**
  *
  * @author jrali
  */
-public class Gitdemo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Earth!");
-    }
+public class StudentModel {
+    private String name;
 }
